e54beb14c136db1ffc401cdd4d1e572fef6de9fe
anilvar <4974886+anilvar@users.noreply.github.com>
2025-03-14 17:40:52 +0530

    Fixing one hot encoding bug
    Fixing one hot encoding bug

--- deeproots/Utils/OneHotEncoding.py
+++ deeproots/Utils/OneHotEncoding.py
@@ -5,26 +5,16 @@
 #No warranty of any kind is provided. Use at your own risk.
 
 #one hot encoding for classification
-def one_hot_encoding(classes_in):
+def one_hot_encoding(classes_in, data):
+
+    nDataPoints = len(data)
+    nClasses    = len(classes_in)
     
-    encoding = np.zeros([len(classes_in), 6])
-    for iC in range(0, len(classes_in)):
-        if(classes_in[iC] == 'Spam'):
-            encoding[iC, 0] = 1
+    encoding = np.zeros([nDataPoints, nClasses])
 
-        if(classes_in[iC] == 'Social Media'):
-            encoding[iC, 1] = 1
 
-        if(classes_in[iC] == 'Work'):
-            encoding[iC, 2] = 1
-        
-        if(classes_in[iC] == 'Marketing'):
-            encoding[iC, 3] = 1
-        
-        if(classes_in[iC] == 'Important'):
-            encoding[iC, 4] = 1
-        
-        if(classes_in[iC] == 'Personal'):
-            encoding[iC, 5] = 1
+    for i in range(0, nDataPoints):
+
+        encoding[i, data[i]] = 1.0
 
     return encoding
